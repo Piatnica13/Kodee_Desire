d6c5deb0b6bd4fb072b0470cb3e394cb452fca3e
Kostya <konstantin.gorkovenko13@gmail.com>
2025-04-22 21:59:41 +0500

    2204НUpdateFromPhone

--- static/js/index.js
+++ static/js/index.js
@@ -95,3 +95,79 @@ if (document.querySelector('#messageForNoReg')){
     showToast("Ошибка авторизации, повторите позже!", 2000)
 }
 
+let back = document.querySelector("#con4galochkaBack"); 
+let lets = document.querySelector("#con4galochkaLet");
+
+let con4part = document.querySelector("#Con4Part1");
+
+lets.addEventListener("click", () => {
+    let con4Width = con4part.offsetWidth;
+    con4part.scrollLeft += con4Width - 14.45;
+    back.style.pointerEvents= `none`;
+    lets.style.pointerEvents= `none`;
+    setTimeout(() => {
+        back.style.pointerEvents= `all`;
+        lets.style.pointerEvents= `all`;
+    }, 400);
+})
+back.addEventListener("click", () => {
+    let con4Width = con4part.offsetWidth;
+    con4part.scrollLeft -= con4Width - 14.45;
+    back.style.pointerEvents= `none`;
+    lets.style.pointerEvents= `none`;
+    setTimeout(() => {
+        back.style.pointerEvents= `all`;
+        lets.style.pointerEvents= `all`;
+    }, 400);
+})
+
+let masFromCon4 = [
+    {title: "Комфорт", info: "Лёгкий и удобный, он не ощущается на запястье, но всегда с тобой"},
+    {title: "Долговечность", info: "Мы используем качественные материалы, чтобы браслет радовал вас долгие годы"},
+    {title: "Минимализм", info: "Лаконичный дизайн, который подходит к любому образу — от повседневного до вечернего"},
+    {title: "Символика", info: "Браслет — это больше, чем украшение. Это история твоих мечтаний и стремлений"},
+    {title: "Надежность", info: "Каждый браслет создан с заботой, чтобы стать твоим верным спутником в любой ситуации"},
+];
+
+function con4func(mas){
+    con4part.innerHTML += 
+    `
+    <div class="Con4Divs" data-aos="fade-up" data-aos-delay="0">
+      <img src="/static/image/auth/show.png" class="con3ImgShow" alt="Посмотреть" style="right: 5%; top: 5%;">
+      <img src="static/image/con4/33784.jpg" alt="" class="Con4Imgs" />
+      <p class="Con4Text Con4TextGl">${mas.title}</p>
+      <p class="Con4Text Con4TextVt">
+        ${mas.info}
+      </p>
+    </div>
+    `;
+};
+
+masFromCon4.forEach((e)=>{
+    con4func(e);
+})
+
+let masFromCon5 = [
+    {title: "Родных", a_filter: "", a_img: ""},
+    {title: "Любимых", a_filter: "", a_img: ""},
+    {title: "Творческих", a_filter: "", a_img: ""},
+    {title: "Пушистых", a_filter: "", a_img: ""},
+    {title: "Спортивных", a_filter: "", a_img: ""},
+    {title: "Миролюбивых", a_filter: "", a_img: ""}
+]
+
+function con5func(mas){
+    let con5 = document.querySelector("#con5grid");
+    con5.innerHTML += `
+    <div class="">
+      <a href="${mas.a_filter}">
+        <p>${mas.title}</p>
+        <img src="${mas.a_img}" alt="">
+      </a>
+    </div>
+    `
+}
+
+masFromCon5.forEach((e)=>{
+    con5func(e);
+})
