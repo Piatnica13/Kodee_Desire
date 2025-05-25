7e498c4a65de7e903f6a04dd70e25e1538c32ce2
Kostya <konstantin.gorkovenko13@gmail.com>
2025-05-25 22:56:53 +0500

    2505KDeployedPartFife

--- run.py
+++ run.py
@@ -3,10 +3,9 @@ from init import allProducts
 from models import Person, Product, Product_image, Address
 
 #Запуск
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        allProducts()
-        add_admin()
-    app.logger.info("Запуск с run.")
-    # app.run()
+with app.app_context():
+    db.create_all()
+    allProducts()
+    add_admin()
+app.logger.info("Запуск с run.")
+# app.run()
